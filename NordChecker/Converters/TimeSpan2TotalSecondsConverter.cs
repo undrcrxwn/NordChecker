c7a2c00c6ca2755e049182f1b8dc0ec84e81f813
undrcrxwn <undrcrxwn@gmail.com>
2021-11-16 21:54:14 +0300

    fix prev commit changes

--- NordChecker/Converters/TimeSpan2TotalSecondsConverter.cs
+++ NordChecker/Converters/TimeSpan2TotalSecondsConverter.cs
@@ -5,7 +5,7 @@ using System.Windows.Data;
 namespace NordChecker.Converters
 {
     [ValueConversion(typeof(TimeSpan), typeof(double))]
-    public class TimeSpan2TotalSecondsConverter1 : IValueConverter
+    public class TimeSpan2TotalSecondsConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
             ((TimeSpan)value).TotalSeconds;
